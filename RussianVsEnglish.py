25e860a864f9d91d3995a3fab9328155ed6ed2e6
cpedroza1 <cpedroza.cp@gmail.com>
2023-09-18 14:44:33 -0600

    added seperate program for just ru vs english

--- RussianVsEnglish.py
+++ RussianVsEnglish.py
@@ -8,13 +8,9 @@ from sklearn.metrics import accuracy_score, classification_report, f1_score
 import collections
 
 
-
 def combine_pred(unigram_probs, model_probs, weight):
     combined_probs = (weight * unigram_probs) + (weight * model_probs)
     return combined_probs
-
-
-    
 
 if __name__ == "__main__":
 
@@ -23,27 +19,10 @@ if __name__ == "__main__":
     new_set = pd.DataFrame
 
     #Dictionary of nationalities associated with a label(number)
-    possibilities = {'Japanese': 1,
-                     'Russian': 2,
-                     'Spanish': 3,
-                     'Italian': 4,
-                     'Chinese': 5,
-                     'Czech': 6, 
-                     'Dutch': 7,
-                     'English': 8,
-                     'French': 9,
-                     'German': 10,
-                     'Greek': 11,
-                     'Irish':12,
-                     'Vietnamese':13,
-                     'Korean':14,
-                     'Portuguese':15,
-                     'Polish':16,
-                     'Arabic':17,
-                     'Scottish': 18}
+    possibilities = {'Russian':1,
+                     'English': 2}
     # reads csv
-    names = pd.read_csv('surnames-test.csv', encoding='utf-8')
-    results = pd.read_csv('surnames-result.csv', encoding='utf-8')
+    names = pd.read_csv('russian-english-dev.txt', encoding='utf-8', sep=',')
 
     #  makes data frame for names column
     names['Name'] = names['Name'].str.lower()
@@ -65,10 +44,11 @@ if __name__ == "__main__":
     # using the model to predict labels for test set
     model_pred = model.predict(test_features > 0.5).astype(int)
 
+
     # want to make a dictionary that will assign a dict of char : probabilities from vectorizer and assigns to nationality number as key -> name_unigram_models[2] = char probs for russian labeled data
     name_unigram_models = {}
 
-    for x in range(19):
+    for x in range(3):
         lang_num = names[names['Language'] == x]
         char_counts = collections.Counter("".join(lang_num['Name']))
         total_chars = sum(char_counts.values())
@@ -76,70 +56,38 @@ if __name__ == "__main__":
         
         name_unigram_models[x] = unigram_probs
 
-#### this is commented out since this is to test all languages.  made another to only test russian names
-
-    # will be able to take name, find sum of character prob, and combine with model predictions to find best possible choice
-    # unigram_pred = []
-
-    # for name in X_test:
-    #     choices = []
-
-    #     for x in range(18):
-    #         unigram_probs = []
-    #         unigram_probs = name_unigram_models[x]
-
-    #         for char in name:
-    #             if char not in name_unigram_models[x]:
-    #                 unigram_probs[char] = 0.0
-            
-    #         possible_nationality = sum([unigram_probs[char] for char in name])
-            
-    #         choices.append(possible_nationality)
-
-        
-    #     predicted_nationality = choices.index(max(choices))
-        
-    #     unigram_pred.append(predicted_nationality)
-        
-    #     # combine predictions  
-    #     final_predictions = [combine_pred(unigram_pred, model_pred, weight=0.5) for unigram_pred, model_pred in zip(unigram_pred, model_pred)]
-    #     final_predictions = np.round(final_predictions).astype(int)
-    #     final_predictions = np.abs(final_predictions)
-
-
-#### this is to predict only using the probability of russian names with the russian character set
-
+    #### this is to predict only using the probability of russian names with the russian character set
     unigram_pred = []
-    russian_unigram_probs = []
-    english_unigram_probs = []
 
     for name in X_test:
         choices = []
 
-        # gets dictionary of (char: probability for russian names).  any missing character is set to 0
-        russian_unigram_probs = name_unigram_models[2]
-        english_unigram_probs = name_unigram_models[8]
-        for char in name:
-            if char not in name_unigram_models[2]:
-                russian_unigram_probs[char] = 0.0
+        for x in range(2):
+            unigram_probs = []
+            unigram_probs = name_unigram_models[x]
 
-        # gets total probability for characters in name
-        total_name_char_prob = sum([russian_unigram_probs[char] for char in name])
+            for char in name:
+                if char not in name_unigram_models[x]:
+                    unigram_probs[char] = 0.0
+            
+            possible_nationality = sum([unigram_probs[char] for char in name])
+            
+            choices.append(possible_nationality)
 
-        # if name probability is above or equal to threshold it w
-        if total_name_char_prob >= 0.4:
-            unigram_pred.append(2)
-        else:
-            unigram_pred.append(0)
+        
+        predicted_nationality = choices.index(max(choices))
+        
+        unigram_pred.append(predicted_nationality)
+        
+        # combine predictions  
+        final_predictions = [combine_pred(unigram_pred, model_pred, weight=0.5) for unigram_pred, model_pred in zip(unigram_pred, model_pred)]
+        final_predictions = np.round(final_predictions).astype(int)
+        final_predictions = np.abs(final_predictions)
 
-        # Combine predictions
-        final_predictions = unigram_pred
-
-#### this is to print reports of accuracy and so on 
-
+    
     # writing results to csv
     results = pd.DataFrame({'Name': X_test, 'Prediction': final_predictions})
-    results.to_csv('surnames-result.csv', index=False)
+    results.to_csv('ru-vs-english-result.csv', index=False)
 
     # prints accuracy score using the test data and model predictions.
     accuracy = accuracy_score(y_test, final_predictions)
@@ -157,7 +105,7 @@ if __name__ == "__main__":
 
 #### printing bigrams for english names and their probabilities 
 
-    target = 8  # change this value to choose which language you want to filter bigrams for
+    target = 2  # change this value to choose which language you want to filter bigrams for
 
     mask = np.array(y_train) == target
     bigram_sums = train_features[mask].sum(axis=0)
@@ -178,9 +126,3 @@ if __name__ == "__main__":
     # print the bigrams and the probability 
     for bigram, probability in bigram_probabilities.items():
         print(f"Bigram: {bigram}, Probability: {probability}")
-
-   
-
-
-
-            
